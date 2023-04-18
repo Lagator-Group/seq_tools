5c485b5e682beea5e6a7a33e295764d8d96aa53f
acidsh0t <91309688+acidsh0t@users.noreply.github.com>
2023-04-18 22:41:29 +0100

    Update assembly.sh

--- assembly.sh
+++ assembly.sh
@@ -1,13 +1,10 @@
-#$ -cwd
+#!/bin/bash
 
 conda activate filtlong
-python3 pwd/seq_tools/filtlong/filtlong.py
-
+python3 seq_tools/filtlong/filtlong.py
 conda activate unicycler
-python3 pwd/seq_tools/unicycler/unicycler.py
-
-conda activate flye
-python3 pwd/seq_tools/flye/flye.py
-
+python3 seq_tools/unicycler/unicycler.py
 conda activate raven
-python3 pwd/seq_tools/raven/raven.py
+python3 seq_tools/raven/raven.py
+conda activate flye
+python3 seq_tools/flye/flye.py

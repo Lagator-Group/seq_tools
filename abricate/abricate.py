193637606ce7a27a31f4e278b4267dd1eb8b28bb
acidsh0t <91309688+acidsh0t@users.noreply.github.com>
2023-04-20 22:52:18 +0100

    Update abricate.py

--- abricate/abricate.py
+++ abricate/abricate.py
@@ -1,6 +1,26 @@
 #! python3
 
 import glob
+import os
+import subprocess
 
-assembly=glob.glob('**/assembly.fasta', recursive=True)
-print(assembly)
+def main():
+    assembly=glob.glob('**/assembly.fasta', recursive=True) #generates list of all "assembly.fasta" in subdirectories
+    csv_list=[]
+
+    for fasta in assembly:
+        name=fasta[:-15]+'.csv'
+        csv_path='abricate/'+name
+        csv_list.append(csv_path)
+        print(name)
+
+        if not os.path.isdir('abricate'):
+            mkdir='mkdir abricate'
+            subprocess.call(mkdir,shell=True)
+        abricate='abricate '+fasta+' > abricate/'+name
+        subprocess.call(abricate,shell=True)
+
+        summary= #converts list into string separated by space    
+
+if __name__=="__main__":
+    main()

e4fd73c63446ff229bdf8be55c26828b908b0113
acidsh0t <91309688+acidsh0t@users.noreply.github.com>
2023-04-21 18:17:50 +0100

    Update assembly_plasmid.sh

--- assembly_plasmid.sh
+++ assembly_plasmid.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+seq_tools="/mnt/c/Users/jtdej/Desktop/GitHub/seq_tools"
+
 conda activate filtlong
-python3 seq_tools/filtlong/filtlong.py
+python3 "${seq_tools}/filtlong/filtlong.py"
 conda activate unicycler
-python3 seq_tools/unicycler/unicycler.py
+python3 "${seq_tools}/unicycler/unicycler.py"
 conda activate raven
-python3 seq_tools/raven/raven.py
+python3 "${seq_tools}/raven/raven.py"
 conda activate flye
-python3 seq_tools/flye/flye.py
+python3 "${seq_tools}/flye/flye_ont_plasmid.py"

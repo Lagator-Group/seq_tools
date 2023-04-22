c7ee444829122e7142b15fe35c150a20f3627043
acidsh0t <91309688+acidsh0t@users.noreply.github.com>
2023-04-22 14:37:02 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+#run by executing "bash -i update.sh"
+
+#filtlong
+conda activate filtlong
+conda update -c bioconda filtlong
+
+#unicycler
+conda activate unicycler
+conda update -c bioconda unicycler
+
+#flye
+conda activate flye
+conda update -c bioconda flye
+
+#raven
+conda activate raven
+conda update -c bioconda raven-assembler
+
+#abricate
+conda activate abricate
+conda update -c bioconda abricate

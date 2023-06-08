c06c5cc7ef0df7f13e62223606100e702183908b
acidsh0t <91309688+acidsh0t@users.noreply.github.com>
2023-06-08 11:27:50 +0100

    fixed bash command

--- RNAseq/trim_galore.py
+++ RNAseq/trim_galore.py
@@ -9,14 +9,14 @@ def main():
     n=0
     seq_list=[]
     for seq in os.listdir(): #groups sequences into short and long reads
-        if seq.endswith('_1.fastq') or seq.endswith('_1fastq.gz'):
+        if seq.endswith('_1.fastq') or seq.endswith('_1.fastq.gz'):
             seq_list.append(seq)
 
     for seq in seq_list:
         _1=str(seq)
         _2=str(_1.replace('_1.fastq','_2.fastq')) #assumes there is a corresponding _2 in folder
 
-        trim_galore='trim_galore --paired '+_1+' '+_2+' -q 20 --phred33 --fastqc --length 50 -o fastq_trimmed/ '+str(threads)
+        trim_galore='trim_galore --paired '+_1+' '+_2+' -q 20 --phred33 --fastqc --length 50 -o fastq_trimmed/ -j '+str(threads)
         subprocess.call(trim_galore,shell=True)
 
         n=n+1

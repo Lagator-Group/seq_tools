afb4fe157edf9fc6957504dca296aee57d556555
acidsh0t <91309688+acidsh0t@users.noreply.github.com>
2023-06-14 15:29:32 +0100

    featurecounts

--- RNAseq/featurecounts.py
+++ RNAseq/featurecounts.py
@@ -6,9 +6,9 @@ import subprocess
 threads=8
 
 def main():
-    for bam in os.listdir('BAM_sorted'):
-        featurecounts='featureCounts -a refseq/genomic.gtf -p -T '+str(threads)+' -t CDS -g gene_id -o featureCounts_table.txt BAM_sorted/'+bam
-        subprocess.call(featurecounts,shell=True)
+    featurecounts='featureCounts -a refseq/genomic.gtf -p -T '+str(threads)+' -t CDS -g gene_id -o featureCounts_table.txt *bam'
+    print(featurecounts)
+    subprocess.call(featurecounts,shell=True)
 
 if __name__ == "__main__":
     main()

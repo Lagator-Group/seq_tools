560e22407a76ce13f27191a1d46fcd5ed75c3adc
acidsh0t <91309688+acidsh0t@users.noreply.github.com>
2023-06-14 14:28:39 +0100

    featurecounts

--- RNAseq/featurecounts.py
+++ RNAseq/featurecounts.py
@@ -3,8 +3,12 @@
 import os
 import subprocess
 
+threads=8
+
 def main():
-    
+    for bam in os.listdir('BAM_sorted'):
+        featurecounts='featureCounts -a refseq/genomic.gtf -p -T '+str(threads)+' -t CDS -g gene_id -o featureCounts_table.txt BAM_sorted/'+bam
+        subprocess.call(featurecounts,shell=True)
 
 if __name__ == "__main__":
     main()

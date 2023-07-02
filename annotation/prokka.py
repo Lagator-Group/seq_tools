9cda0a835c9848d5437d6618010e109f565bbace
acidsh0t <91309688+acidsh0t@users.noreply.github.com>
2023-07-02 21:05:47 +0100

    prokka fix

--- annotation/prokka.py
+++ annotation/prokka.py
@@ -8,14 +8,23 @@ import shutil
 currently doesnt work on UoM CSF. issue with prokka and blastp
 '''
 
+def prokka(file):
+    name=file.replace('.fasta','')
+    folder='prokka_plasmid_'+name
+
+    prokka='prokka --outdir '+folder+' --prefix '+name+' '+file
+    subprocess.call(prokka,shell=True)
+
 def main():
     if os.path.isdir('contigs_plasmid'):
         for file in os.listdir('contigs_plasmid'):
-            name=file.replace('.fasta','')
-            folder='prokka_plasmid_'+name
+            if file.endswith('.fasta'):
+                prokka(file)
+    else:
+        for file in os.listdir():
+            if file.endswith('.fasta'):
+                prokka(file)
 
-            prokka='prokka --outdir '+folder+' --prefix '+name+' '+file
-            subprocess.call(prokka,shell=True)
 
 if __name__=="__main__":
     main()

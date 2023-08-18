b5216b77d446b7d280dc28161887d69ef1a22ec5
acidsh0t <91309688+acidsh0t@users.noreply.github.com>
2023-08-18 14:48:11 +0100

    uninstall

---
+++ ngs_uninstall.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#run by executing "bash -i ngs_setup.sh"
+
+#filtlong
+conda remove -n filtlong --all
+
+#unicycler
+conda remove -n unicycler --all
+
+#abricate
+conda remove -n abricate --all
+
+#prokka
+conda remove -n prokka --all
---
+++ rnaseq_uninstall.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#run by executing "bash -i rnaseq_setup.sh"
+
+#trim-galore
+conda remove -n trim-galore --all
+
+#bbmap
+conda remove -n bbmap --all
+
+#seqkit
+conda remove -n seqkit --all
+
+#bowtie
+conda remove -n bowtie --all
+
+#samtools
+conda remove -n samtools --all
+
+#subread
+conda remove -n subread --all
